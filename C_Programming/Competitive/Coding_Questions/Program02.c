55cc42f3e63fc4fa4601533afa52e41e0d89bafc
Samiksha Shinde <166107150+samiksharshinde@users.noreply.github.com>
2026-06-20 10:33:36 +0530

    Add files via upload

---
+++ C_Programming/Competitive/Coding_Questions/Program02.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+//Program to print 5 times "Marvellous" on screen.
+void Display()
+{
+    int i=0;
+    for(i=1;i<=5;i++)
+    {
+        printf("Marvellous\n");
+    }
+}
+
+int main()
+{
+    Display();
+    return 0;
+}
