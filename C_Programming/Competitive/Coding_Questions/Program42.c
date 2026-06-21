1b58f108df9619065cca15787e753282de43437e
Samiksha Shinde <166107150+samiksharshinde@users.noreply.github.com>
2026-06-21 21:58:58 +0530

    Add files via upload

---
+++ C_Programming/Competitive/Coding_Questions/Program42.c
@@ -0,0 +1,29 @@
+//write program which accept number from user and return the count of odd digits.
+
+#include<stdio.h>
+int CountOdd(int iNo)
+{
+    int iDigit=0;
+    int iCnt=0;
+    while(iNo != 0)
+    {
+        iDigit=iNo%10;
+        if(iDigit%2!=0)
+        {
+            iCnt++;
+        }
+        iNo=iNo/10;
+    }
+    return iCnt;
+}
+int main()
+{
+    int iValue=0;
+    int iRet=0;
+    printf("Enter the number:");
+    scanf("%d",&iValue);
+    iRet=CountOdd(iValue);
+    printf("%d",iRet);
+    return 0;
+
+}
---
+++ C_Programming/Competitive/Coding_Questions/Program44.c
@@ -0,0 +1,32 @@
+//write a program which accept number from user and return multiplication of all digits.
+
+#include<stdio.h>
+int MultDigits(int iNo)
+{
+    
+    int iDigit=0;
+    int iMult=1;
+    
+    while(iNo != 0)
+    { 
+        iDigit=iNo%10;
+        if(iDigit != 0)
+        {
+            iMult=iMult*iDigit;
+        }
+        
+        iNo=iNo/10;
+    }
+    return iMult;
+}
+int main()
+{
+    int iValue=0;
+    int iRet=0;
+    printf("Enter the number:");
+    scanf("%d",&iValue);
+    iRet=MultDigits(iValue);
+    printf("%d",iRet);
+    return 0;
+
+}
