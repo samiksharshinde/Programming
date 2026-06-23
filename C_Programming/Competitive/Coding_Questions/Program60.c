322cf5557c425d18662b468a6fa5baffcdb084da
Samiksha Shinde <166107150+samiksharshinde@users.noreply.github.com>
2026-06-23 13:34:48 +0530

    Add files via upload

---
+++ C_Programming/Competitive/Coding_Questions/Program60.c
@@ -0,0 +1,30 @@
+//Write a program which accept from user and count frequency of such digits which are less than 6.
+#include<stdio.h>
+
+int Count(int iNo)
+{
+    int iCnt=0;
+    int iDigit=0;
+    while(iNo != 0)
+    {
+        iDigit=iNo%10;
+
+        if(iDigit<6)
+        {
+            iCnt++;
+        }
+        iNo=iNo/10;
+    }
+    return iCnt;
+
+}
+int main()
+{
+    int iValue=0;
+    int iRet=0;
+    printf("Enter number:");
+    scanf("%d",&iValue);
+    iRet=Count(iValue);
+    printf("%d",iRet);
+    return 0;
+}
