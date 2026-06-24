f3fc32ab67a660f1ecff6f50a661f4dda040eb75
Samiksha Shinde <166107150+samiksharshinde@users.noreply.github.com>
2026-06-24 22:19:36 +0530

    Add files via upload

---
+++ C_Programming/Competitive/Coding_Questions/program89.c
@@ -0,0 +1,40 @@
+/*Accept number of rows and number of columns from user and display below pattern
+Input: iRow=3 iCol=4
+Output: * # * #
+        * # * #
+        * # * #
+*/
+
+#include<stdio.h>
+
+void pattern(int iRow,int iCol)
+{
+        int i=0,j=0;
+        for(i=1;i<=iRow;i++)
+        {
+                for(j=1;j<=iCol;j++)
+                {
+                        if(j%2!=0)
+                        {
+                                printf("*\t");
+                        }
+                        else
+                        {
+                                printf("#\t");
+                        }
+                           
+                }
+                printf("\n");
+        }
+
+}
+
+int main()
+{
+        int iValue1=0,iValue2=0;
+        printf("Enter the number of rows:");
+        scanf("%d",&iValue1);
+        printf("Enter the number of columns");
+        scanf("%d",&iValue2);
+        pattern(iValue1,iValue2);
+}
---
+++ C_Programming/Competitive/Coding_Questions/program90.c
@@ -0,0 +1,34 @@
+/*Accept number of rows and number of columns from user and display below pattern
+Input: iRow=4 iCol=4
+Output: 1 1 1 1
+        2 2 2 2
+        3 3 3 3
+        4 4 4 4
+*/
+
+#include<stdio.h>
+
+void pattern(int iRow,int iCol)
+{
+        int i=0,j=0;
+        for(i=1;i<=iRow;i++)
+        {
+                for(j=1;j<=iCol;j++)
+                {
+                        printf("%d\t",i);
+                           
+                }
+                printf("\n");
+        }
+
+}
+
+int main()
+{
+        int iValue1=0,iValue2=0;
+        printf("Enter the number of rows:");
+        scanf("%d",&iValue1);
+        printf("Enter the number of columns:");
+        scanf("%d",&iValue2);
+        pattern(iValue1,iValue2);
+}
