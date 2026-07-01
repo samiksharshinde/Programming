872be1ba46cac932d347f34c468bda7403b2d21d
Samiksha Shinde <166107150+samiksharshinde@users.noreply.github.com>
2026-07-01 22:06:37 +0530

    Add files via upload

---
+++ C_Programming/Competitive/Coding_Questions/program105.c
@@ -0,0 +1,56 @@
+/*Accept number of rows and numbers of columns from user and display below pattern
+INPUT: iRow=4 iCol=4
+OUTPUT: 1 2 3 4 
+        2 * * 5
+        3 * * 6
+        4 5 6 7
+
+*/
+
+#include<stdio.h>
+
+void pattern(int iRow,int iCol)
+{
+        int i=0,j=0;
+        
+        for(i=1;i<=iRow;i++)
+        {
+                for(j=1;j<=iCol;j++)
+                {
+                    if(i==1)
+                    {
+                       
+                         printf("%d\t",j);
+                         
+                    }
+                    else if(j==1)
+                    {
+                        printf("%d\t",i);   
+                    }
+                    else if(j==iCol || i==iRow)
+                    {
+                        printf("%d\t",i+j-1);
+                    }
+                    else
+                    {
+                        printf("*\t");
+                    }
+                   
+                       
+                       
+                       
+                       
+                }
+                printf("\n");
+        }
+
+}
+int main()
+{
+        int iValue1=0,iValue2=0;
+        printf("Enter the number of rows:");
+        scanf("%d",&iValue1);
+        printf("Enter the number of columns:");
+        scanf("%d",&iValue2);
+        pattern(iValue1,iValue2);
+}
